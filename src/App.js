e73090b6e8abe669f713a2d3a8a79c59155e6e6c
rei_kaji <st.r.z.rei@gmail.com>
2022-05-03 11:19:34 -0700

    fixed event-detail ui

--- src/App.js
+++ src/App.js
@@ -5,7 +5,17 @@ function App() {
   return (
     <div className="App">
       <h1>soccer-event-bbs</h1>
-      <EventDetail />
+      <EventDetail 
+        name="EventNameA"
+        img="https://source.unsplash.com/random"
+        date="xx:xx"
+        adress="aaa"
+        adress_link="https://www.google.com/maps/search/?api=1&parameters"
+        genre="soccer"
+        attendees="100"
+        last_update="xx:xx"
+        detail="xxxxxxxXxxxxxxxx"
+      />
     </div>
   );
 }
--- src/components/EventDetail.js
+++ src/components/EventDetail.js
@@ -6,7 +6,6 @@ import CardMedia from '@mui/material/CardMedia';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import RoomIcon from '@mui/icons-material/Room';
 import SportsSoccerIcon from '@mui/icons-material/SportsSoccer';
@@ -15,81 +14,83 @@ import SportsSoccerIcon from '@mui/icons-material/SportsSoccer';
 const cards = [1];
 
 export default function EventDetail(props){
-    
-
     return(
         <div>
-            <Container sx={{ py: 8 }} maxWidth="md">
-                {/* End hero unit */}
-                <Grid xs={12} sm={6} md={4}>
+            {/* Event Card Create */}
+            <Container sx={{ py: 8 }} maxWidth="md" style={{paddingTop:10,paddingBottom:0}} >
                 <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
-                            <CardContent sx={{ flexGrow: 1 }}>
-                                <Typography>
-                                    <h3>EventName</h3>
-                                </Typography>
-                            </CardContent>
-                            <CardMedia
-                                component="img"
-                                image="https://source.unsplash.com/random"
-                                alt="random"
-                                height="250px"
-                            />
-                            <CardContent sx={{ flexGrow: 1 }}>
-                                <Typography paragraph>
-                                    <div className="event_info">
-                                        <div>
-                                            <div className="event_info_group">
-                                                <div className="event_info_date">
-                                                    <AccessTimeIcon/>
-                                                    Event Date
-                                                </div>
-                                                <div className="event_info_count">
-                                                    Attendees XX
-                                                </div>
-                                            </div>
-                                            <Link className="event_info_adress" component="button" variant="body2" onClick={() => {console.info("I'm a button.");}}>
-                                                <RoomIcon/>Event Adress
-                                            </Link>
-                                            <div className="event_info_genre">
-                                                <SportsSoccerIcon/>
-                                                Event Genre
-                                            </div>
+                    {/* Event Title */}
+                    <CardContent sx={{ flexGrow: 1 }} style={{paddingTop:0,paddingBottom:0}}>
+                        <Typography>
+                            <h3>{props.name}</h3>
+                        </Typography>
+                    </CardContent>
+                    {/* Event Image */}
+                    <CardMedia
+                        component="img"
+                        image={props.img}
+                        objectFit="contain"
+                    />
+                    {/* Event Info */}
+                    <CardContent sx={{ flexGrow: 1 }} style={{paddingBottom:0}}>
+                        <Typography paragraph>
+                            <div className="event_info">
+                                <div>
+                                    <div className="event_info_group">
+                                        <div className="event_info_date">
+                                            <AccessTimeIcon className="event_info_icon" fontSize="medium"/>
+                                            <span className="event_info_span">{props.date}</span>
+                                        </div>
+                                        <div className="event_info_count">
+                                            Attendees: {props.attendees} people
                                         </div>
                                     </div>
-                                    <div className="event_info_updatedate">Last Update: XX:XXpm</div>
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                </Grid>
+                                    <div className="event_info_adress" >
+                                        <RoomIcon className="event_info_icon" fontSize="medium"/>
+                                        <a target="_blank" href={props.adress_link}>{props.adress}</a>
+
+                                    </div>
+                                    
+                                    <div className="event_info_genre">
+                                        <SportsSoccerIcon className="event_info_icon" fontSize="medium"/>
+                                        <span  className="event_info_span">{props.genre}</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="event_info_updatedate">Last Update: {props.last_update}</div>
+                        </Typography>
+                    </CardContent>
+                </Card>
             </Container>
-            <Container sx={{ py: 8 }} maxWidth="md">
+            {/* Event Explanation */}
+            <Container sx={{ py: 8 }} maxWidth="md" style={{paddingTop:10,paddingBottom:0}}>
                 {/* End hero unit */}
                 <Grid xs={12} sm={6} md={4}>
                         <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
-                            <CardContent sx={{ flexGrow: 1 }}>
+                            <CardContent className="event_detail"  sx={{ flexGrow: 1 }} style={{paddingTop:0,paddingBottom:0}}>
                                 <Typography gutterBottom variant="h5" component="h2">
                                     <h3>Explanation of this Event</h3>
                                     <p>
-                                        XXXXXXXXXXXXXXXXxXX
-                                        <br></br>XXXXX
+                                        {props.detail}
                                     </p>
                                 </Typography>
                             </CardContent>
                         </Card>
                     </Grid>
             </Container>
-            <Container sx={{ py: 8 }} maxWidth="md">
-                    <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column'}}>
-                        <div className="event_status">
-                            <p className="event_status_title">We're now...</p>
-                            <input className="event_status_input"></input>
-                            <p className="event_status_item">people</p>
-                        </div>
-                    </Card>
+            {/* Event Attendees Count Input */}
+            <Container sx={{ py: 8 }} maxWidth="md" style={{paddingTop:10,paddingBottom:15}}>
+                <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column'}}>
+                    <div className="event_status">
+                        <p className="event_status_title">We're now...</p>
+                        <input type="number" max="100" className="event_status_input"></input>
+                        <p className="event_status_item">people</p>
+                    </div>
+                </Card>
             </Container>
-
+            {/* Event Detail Update */}
             <div className="event_info_updatebutton">
-                        <Button variant="contained" size="large">Update</Button>
+                 <Button variant="contained" sx={"background-color:#AE303A;"} style={{maxWidth: '200px', minWidth: '200px'}}>Update</Button>
             </div>
         </div>
 
